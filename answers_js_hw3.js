48164e4386a2363121489053ebba9cdb72f65510
Svitlana Rzhanovska <rzhanovska_s@pbi.office>
2024-11-05 21:46:47 +0200

    HW

---
+++ answers_js_hw3.js
@@ -0,0 +1,28 @@
+//answers hw_3
+//або 
+
+function pow(x, y) {
+    let result = 1;
+    for (let i = 0; i < y; i++) {
+        result *= x;
+    }
+    return result;
+}
+
+
+console.log (pow(2, 5))
+
+
+//або 
+function pow(x, y) {
+    let result = 1;
+    for (let i = 0; i < y; i++) {
+        result *= x;
+    }
+    return result;
+}
+let result = pow (3,  2)
+
+console.log (result)
+
+
